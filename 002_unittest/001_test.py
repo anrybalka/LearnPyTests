e27f156a3646c332bfa4d2b52fb6de46deea0fcd
Rybalka Andrey Anatolyevich <anrybalka@o3.ru>
2025-02-01 10:42:47 +0500

    unittest update

--- 002_unittest/001_test.py
+++ 002_unittest/001_test.py
@@ -3,7 +3,11 @@ import unittest
 class MyTest(unittest.TestCase):
     def test1(self):
         sum_ = 2+2
-        assert sum_ == 5
+        assert sum_ == 4
+
+    def test2(self):
+        sum_ = 2+2
+        self.assertEqual (sum_,4)    
 
 # if __name__ == '__main__':
 #     unittest.main()
